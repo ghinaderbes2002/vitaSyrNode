6a5e536beb99820c4c06b53eaa414841345eb4fa
ghinaderbes2002 <ghinaderbes@gmail.com>
2025-12-04 15:34:19 +0300

    update

--- init-db.sh
+++ init-db.sh
@@ -10,8 +10,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     -- Ensure UTF8 encoding
     UPDATE pg_database SET datistemplate = FALSE WHERE datname = '$POSTGRES_DB';
 
-    -- Grant all privileges
-    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+    -- Grant all privileges (using quoted identifier for case-sensitive database name)
+    GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO $POSTGRES_USER;
 
     -- Connect to the database and set up schema permissions
     \c $POSTGRES_DB
