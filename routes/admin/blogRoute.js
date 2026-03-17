a99c11a20f5d7e0ec5e2ef5a0a100f2cbb9527d3
ghinaderbes2002 <ghinaderbes@gmail.com>
2026-03-17 16:09:24 +0300

    ..

--- routes/admin/blogRoute.js
+++ routes/admin/blogRoute.js
@@ -19,7 +19,7 @@ const router = express.Router();
 // =======================
 // الفئات
 // =======================
-router.get("/blog-categories", isAdmin, getAllCategories);
+router.get("/blog-categories", getAllCategories);
 router.post("/blog-categories", verifyToken, isAdmin, createCategory);
 router.put("/blog-categories/:id", verifyToken, isAdmin, updateCategory);
 router.delete("/blog-categories/:id", verifyToken, isAdmin, deleteCategory);
@@ -31,13 +31,14 @@ router.get("/blog-posts",  getAllPosts);
 
 router.post(
   "/blog-posts",
+  verifyToken,
   isAdmin,
   upload.single("featuredImage"),
   compressImage,
   createPost
 );
 
-router.get("/blog-posts/:id",  isAdmin, getPostById);
+router.get("/blog-posts/:id", verifyToken, isAdmin, getPostById);
 
 router.put(
   "/blog-posts/:id",
